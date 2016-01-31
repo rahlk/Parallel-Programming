0e63b5c3adacbc5ca815a5ce20fdf4eac688fabb
rahlk <i.m.ralk@gmail.com>
2016-01-31 15:09:59 -0500

    compute node commit...

--- src/hw2/mympi/c_socket/client.c
+++ src/hw2/mympi/c_socket/client.c
@@ -1,57 +1,60 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
+#include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <netdb.h> 
+#include <string.h>
+#include <arpa/inet.h>
 
-void error(const char *msg)
+
+#define MAXLINE 4096 /*max text line length*/
+#define SERV_PORT 3000 /*port*/
+
+int
+main(int argc, char **argv) 
 {
-    perror(msg);
-    exit(0);
-}
+ int sockfd;
+ struct sockaddr_in servaddr;
+ char sendline[MAXLINE], recvline[MAXLINE];
+	
+ //basic check of the arguments
+ //additional checks can be inserted
+ if (argc !=2) {
+  perror("Usage: TCPClient <IP address of the server"); 
+  exit(1);
+ }
+	
+ //Create a socket for the client
+ //If sockfd<0 there was an error in the creation of the socket
+ if ((sockfd = socket (AF_INET, SOCK_STREAM, 0)) <0) {
+  perror("Problem in creating the socket");
+  exit(2);
+ }
+	
+ //Creation of the socket
+ memset(&servaddr, 0, sizeof(servaddr));
+ servaddr.sin_family = AF_INET;
+ servaddr.sin_addr.s_addr= inet_addr(argv[1]);
+ servaddr.sin_port =  htons(SERV_PORT); //convert to big-endian order
+	
+ //Connection of the client to the socket 
+ if (connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr))<0) {
+  perror("Problem in connecting to the server");
+  exit(3);
+ }
+	
+ while (fgets(sendline, MAXLINE, stdin) != NULL) {
+	
+  send(sockfd, sendline, strlen(sendline), 0);
+		
+  if (recv(sockfd, recvline, MAXLINE,0) == 0){
+   //error: server terminated prematurely
+   perror("The server terminated prematurely"); 
+   exit(4);
+  }
+  printf("%s", "String received from the server: ");
+  fputs(recvline, stdout);
+ }
 
-int main(int argc, char *argv[])
-{
-    int sockfd, portno, n;
-    struct sockaddr_in serv_addr;
-    struct hostent *server;
-
-    char buffer[256];
-    if (argc < 3) {
-       fprintf(stderr,"usage %s hostname port\n", argv[0]);
-       exit(0);
-    }
-    portno = atoi(argv[2]);
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) 
-        error("ERROR opening socket");
-    server = gethostbyname(argv[1]);
-    if (server == NULL) {
-        fprintf(stderr,"ERROR, no such host\n");
-        exit(0);
-    }
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr, 
-         (char *)&serv_addr.sin_addr.s_addr,
-         server->h_length);
-    serv_addr.sin_port = htons(portno);
-    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
-        error("ERROR connecting");
-    printf("Please enter the message: ");
-    bzero(buffer,256);
-    fgets(buffer,255,stdin);
-    n = write(sockfd,buffer,strlen(buffer));
-    if (n < 0) 
-         error("ERROR writing to socket");
-    bzero(buffer,256);
-    n = read(sockfd,buffer,255);
-    if (n < 0) 
-         error("ERROR reading from socket");
-    printf("%s\n",buffer);
-    close(sockfd);
-    return 0;
+ exit(0);
 }
