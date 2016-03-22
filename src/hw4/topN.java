5b0286487fd196143d51d0ae88c4413f518ed065
rahlk <i.m.ralk@gmail.com>
2016-03-21 21:24:37 -0400

    autocommit from macbook

--- src/hw4/topN.java
+++ src/hw4/topN.java
@@ -1,1 +1,95 @@
-//
+package wc;
+
+import java.io.IOException;
+import java.util.*;
+
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.fs.*;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.lib.input.*;
+import org.apache.hadoop.mapreduce.lib.output.*;
+import org.apache.hadoop.util.*;
+
+public class WordCount extends Configured implements Tool {
+
+  public static void main(String args[]) throws Exception {
+    // Get input arguments and make sure they are sufficient
+    Configuration conf = new Configuration();
+    String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+    if (otherArgs.length != 3) {
+    System.err.println("Usage: TopWord <in> <out> <N>");
+    System.exit(3);
+    }
+    // Set up the driver class
+    int res = ToolRunner.run(new WordCount(), args);
+    System.exit(res);
+  }
+
+  public int run(String[] args) throws Exception {
+
+    // Extract input and output path from cmdline args
+    Path inputPath = new Path(args[0]);
+    Path outputPath = new Path(args[1]);
+
+    Configuration konf = getConf();
+
+    // Create a JobConf Object
+    Job job = new Job(konf, this.getClass().toString());
+
+    // Read the paths as an HDFS complient path
+    FileInputFormat.setInputPaths(job, inputPath);
+    FileOutputFormat.setOutputPath(job, outputPath);
+
+    // Create a Job name for reference
+    job.setJobName("WordCount");
+    // Tell MapReduce what to look for in a executabe jar file
+    job.setJarByClass(WordCount.class);
+    //
+    job.setInputFormatClass(TextInputFormat.class);
+    job.setOutputFormatClass(TextOutputFormat.class);
+    // Tell Hadoop input/output key/value data type
+    job.setMapOutputKeyClass(Text.class);
+    job.setMapOutputValueClass(IntWritable.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+    // Set mapper and reducer classes
+    job.setMapperClass(Map.class);
+    job.setCombinerClass(Reduce.class);
+    job.setReducerClass(Reduce.class);
+
+    // Return status to main
+    return job.waitForCompletion(true) ? 0 : 1;
+  }
+
+  public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
+    private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+
+    @Override
+    public void map(LongWritable key, Text value,
+                    Mapper.Context context) throws IOException, InterruptedException {
+      String line = value.toString();
+      StringTokenizer tokenizer = new StringTokenizer(line);
+      while (tokenizer.hasMoreTokens()) {
+        word.set(tokenizer.nextToken());
+        context.write(word, one);
+      }
+    }
+  }
+
+  public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+    @Override
+    public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+      int sum = 0;
+      for (IntWritable value : values) {
+        sum += value.get();
+      }
+
+      context.write(key, new IntWritable(sum));
+    }
+  }
+
+}
